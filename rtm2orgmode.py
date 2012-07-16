93e4d0b4dcfd1c77b89c755dca04b49e1c8008a8
Angus Gratton <gus@projectgus.com>
2012-07-16 18:20:19 +1000

    Run through pyflakes & PEP-8

--- rtm2orgmode.py
+++ rtm2orgmode.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import time, datetime, sys
-import bottle, milky
+import datetime
+import bottle
+import milky
 
 # you'll need a separate file api_key.py that assigns
 # API_KEY = "myapikey"
 # API_SECRET = "myapisharedsecret"
 from api_key import API_KEY, API_SECRET
+
 
 @bottle.route("/")
 def export():
@@ -15,19 +17,24 @@ def export():
         return
     lists = api.lists.getList()
     bottle.response.content_type = 'text/plain; charset="utf-8"'
-    yield u"# %d RTM lists exported by rtm2orgmode %s\n" % (len(lists), datetime.datetime.now().isoformat())
+    yield u"# %d RTM lists exported by rtm2orgmode %s\n" % (
+        len(lists),
+        datetime.datetime.now().isoformat()
+        )
     yield u"#\n"
-    for task_list in sorted(lists, key=lambda l:(l.position,l.name)):
+    sort_lists = lambda l: (l.position, l.name)
+    for task_list in sorted(lists, key=sort_lists):
         if task_list.deleted or task_list.smart:
             continue
-        list_todo = ""
         tasks = api.tasks.getList(list_id=task_list.id).lists[0].taskseries
         if len(tasks) == 0:
             continue
         yield u"* %s%s\n" % ("DONE " if task_list.archived else "",
                            task_list.name.strip())
-        # sort tasks to show non-closed first, then newest closed to oldest closest
-        sort_key = lambda task: (datetime.datetime.max-(task.task[0].completed or datetime.datetime.max), task.name.strip())
+        # sort tasks to show non-closed first, then by newest closed
+        sort_key = lambda task: (datetime.datetime.max -
+                                 (task.task[0].completed or
+                                  datetime.datetime.max), task.name.strip())
         for task in sorted(tasks, key=sort_key):
             if task.task[0].deleted:
                 continue
@@ -35,37 +42,41 @@ def export():
                                    task.name.strip(),
                                    orgify_tags(fix_tags(task.tags)))
             if task.task[0].completed:
-                yield u"   CLOSED: [%s]\n" % (task.task[0].completed.strftime("%Y-%m-%d %A %H:%M"))
+                ts = task.task[0].completed.strftime("%Y-%m-%d %A %H:%M")
+                yield u"   CLOSED: [%s]\n" % (ts)
             if len(task.notes):
-                notes = "\n".join([ note.text for note in task.notes ]).split("\n")
+                notes = [note.text for note in task.notes]
+                # split out any embedded newlines
+                notes = "\n".join(notes).split("\n")
                 for note in notes:
                     yield "   %s\n" % (note.strip())
 
 
 def authenticate(frob):
     api = milky.API(API_KEY, API_SECRET, frob=frob)
-    if frob is not None: # verify the frob
+    if frob is not None:      # verify the frob
         try:
-            token = api.get_token()
-            return api # good frob
+            api.get_token()
+            return api        # good frob
         except:
-            api.frob = None # bad frob
+            api.frob = None   # bad frob
     bottle.redirect(api.get_auth_url())
     return None
+
 
 def orgify_tags(tags):
     if len(tags):
         return ":%s:" % ":".join(tags)
     return ""
 
+
 def fix_tags(tags):
-    """ There's a bug in the milky tags type where if there's only one tag, it comes up as a
-    list of characters """
+    """ There's a bug in the milky tags type where if there's
+    only one tag, it comes up as a list of characters. """
     if all(len(a) == 1 for a in tags):
         s = "".join(tags)
-        return [ s ] if len(s) > 0 else [ ]
+        return [s] if len(s) > 0 else []
     return tags
-
 
 
 if __name__ == "__main__":
